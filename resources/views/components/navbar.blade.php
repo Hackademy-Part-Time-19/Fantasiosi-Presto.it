5d58e6b7911e14d552706128848c7ff850d847ec
woweya <mestacca@gmail.com>
2024-03-26 19:11:54 +0100

    navbar cambiata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,14 +5,12 @@
             <img class="img-fluid me-2" style="border-radius: 50%" width="50"
                 src="{{ Storage::url('immagini/logo.png') }}" alt="Immagine Logo">
             <div style="border-right: 1px solid var(--gray-text-color);">
-<<<<<<< HEAD
+
             <a class="navbar-brand me-auto" style="font-weight: 500; text-transform: uppercase; padding-right: 1rem; color:white;" href="{{route('welcome')}}">Presto.it</a>
         </div>
-=======
-                <a class="navbar-brand me-auto" style="font-weight: 500; text-transform: uppercase; padding-right: 1rem"
-                    href="{{ route('welcome') }}">Presto.it</a>
-            </div>
->>>>>>> 1e94d5bc549f64952f5e67429553baa6b02cc908
+
+
+
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                 <li class="nav-item">
