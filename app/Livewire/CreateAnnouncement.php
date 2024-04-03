f9109c3045fbd29f94e0b69f4eb1a851a69fe14c
MattiaSen <147874985+MattiaSen@users.noreply.github.com>
2024-04-03 21:06:12 +0200

    userStory5

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -2,18 +2,21 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
-use Livewire\Attributes\Validate;
-use App\Models\Announcement;
 use App\Models\User;
+use Livewire\Component;
 use App\Models\Category;
+use App\Models\Announcement;
+use Livewire\WithFileUploads;
+use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 
 class CreateAnnouncement extends Component
 {
+    use WithFileUploads;
     protected $user_id;
-   #[Validate('required', message: "Inserisci la categoria!")]
+    #[Validate('required', message: "Inserisci la categoria!")]
     public $category;
     #[Validate('required', message: "Per favore inserisci il titolo dell'articolo")]
     #[Validate('min:5', message: "Inserisci un titolo piu' lungo")]
@@ -25,6 +28,22 @@ class CreateAnnouncement extends Component
     #[Validate('decimal:2', message: "Inserisci un prezzo valido")]
     public $price;
 
+    public $temporary_images;
+    public $images = [];
+   
+
+    protected $rules = [
+        'temporary_images.*' => 'image|max:1024',
+        'images.*' => 'image|max:1024',
+    ];
+
+    protected $messages = [
+        'temporary_images.image' => 'Il file deve essere un immagine.',
+        'temporary_images.max' => 'Il file non può superare i 1 mb.',
+        'images.image' => 'Il file deve essere un immagine.',
+        'images.max' => 'Il file non può superare i 1 mb.',
+    ];
+
 
 
     public function render()
@@ -33,25 +52,56 @@ class CreateAnnouncement extends Component
         return view('livewire.create-announcement');
     }
 
-    public function store(User $user)
-    {   
+    public function store()
+    {
+        $this->validate();
         $category = Category::find($this->category);
+        
 
-        $category->announcements()->create([
+
+
+        $announcement = $category->announcements()->create([
             'title' => $this->title,
             'body' => $this->body,
             'price' => $this->price,
-            'user_id'=>auth()->user()->id,
+            'user_id' => auth()->user()->id,
         ]);
+        if (count($this->images)){
+            foreach ($this->images as $image){
+                $directory = 'announcements/'.$announcement->id;
+               
+                $announcement->images()->create([
+                    'path' => $image->store($directory, 'public'),
+                ]);
+            }
+        }
+        File::deleteDirectory(storage_path('app/livewire-tmp'));
 
-
-        /*$this->validate();
-
-
-
-        $data = ['title' => $this->title, 'body' => $this->body, 'price' => $this->price, 'category_id' =>$this->category->id];
-        Announcement::create($data); */
-
+$this->cleanForm();
         session()->flash('success', 'Annuncio caricato con successo');
+    }
+    public function updatedTemporaryImages()
+    {
+        if ($this->validate(['temporary_images.*' => 'image|max:1024'])) {
+            foreach ($this->temporary_images as $image) {
+                $this->images[] = $image;
+            }
+        }
+    }
+
+    public function updated($property){
+        $this->validateOnly($property);
+        
+    }
+
+    public function cleanForm(){
+        $this->reset();
+    }
+
+    public function removeImage($key)
+    {
+        if (in_array($key, array_keys($this->images))) {
+            unset($this->images[$key]);
+        }
     }
 }
