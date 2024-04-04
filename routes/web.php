b09755a2a5b20be69353238b8c269f78900ecc3b
GabLux <gabrielelucchetti@live.it>
2024-04-04 20:14:20 +0200

    lingua ui implementata

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use App\Http\Controllers\AnnouncementsController;
 |
 */
 //Rotta Welcome
-Route::get('/',[FrontController::class, 'welcome'])->name('welcome');
+Route::get('/', [FrontController::class, 'welcome'])->name('welcome');
 
 
 
 //Rotta Category
-Route::get('/category/announcement/{category}',[FrontController::class,'categoryShow'])->name('category.show');
+Route::get('/category/announcement/{category}', [FrontController::class, 'categoryShow'])->name('category.show');
 
 
 
@@ -30,35 +30,34 @@ Route::get('/category/announcement/{category}',[FrontController::class,'category
 Route::get('announcements', [AnnouncementsController::class, 'index'])->name('announcements.index');
 Route::resource('announcements', AnnouncementsController::class);
 Route::middleware(['auth'])->group(function () {
-    Route::get('/announcement/{announcement}',[FrontController::class,'show'])->name('announcement.show');
+    Route::get('/announcement/{announcement}', [FrontController::class, 'show'])->name('announcement.show');
     Route::resource('announcements', AnnouncementsController::class)->only('create');
-
 });
 
 
 
 //Rotte Revisor
-Route::middleware(['isRevisor'])->group(function(){
-// Home revisore
-Route::get('/revisor/home',[RevisorController::class,'index'])->name('revisor.index');
+Route::middleware(['isRevisor'])->group(function () {
+    // Home revisore
+    Route::get('/revisor/home', [RevisorController::class, 'index'])->name('revisor.index');
 
-//Accetta annuncio
-Route::patch('/accetta/annuncio/{announcement}',[RevisorController::class,'acceptAnnouncement'])->name('revisor.accept');
+    //Accetta annuncio
+    Route::patch('/accetta/annuncio/{announcement}', [RevisorController::class, 'acceptAnnouncement'])->name('revisor.accept');
 
-//Rifiuta annuncio
-Route::patch('/rifiuta/annuncio/{announcement}',[RevisorController::class,'rejectAnnouncement'])->name('revisor.reject');
+    //Rifiuta annuncio
+    Route::patch('/rifiuta/annuncio/{announcement}', [RevisorController::class, 'rejectAnnouncement'])->name('revisor.reject');
 });
 
 //Rotte MAKE Revisor
 
-Route::get('/richiesta/revisore',[RevisorController::class, 'becomeRevisor'])->middleware('auth')->name('become.revisor');
+Route::get('/richiesta/revisore', [RevisorController::class, 'becomeRevisor'])->middleware('auth')->name('become.revisor');
 
-Route::get('/rendi/revisore/{user}',[RevisorController::class, 'makeRevisor'])->name('make.revisor');
+Route::get('/rendi/revisore/{user}', [RevisorController::class, 'makeRevisor'])->name('make.revisor');
 
 
 //ROTTE MAIL
 
-Route::get('/mail',[MailController::class, 'revisorForm'])->name('revisor.form');
+Route::get('/mail', [MailController::class, 'revisorForm'])->name('revisor.form');
 
 Route::post('invio/mail', [MailController::class, 'sendMail'])->name('send.mail');
 
@@ -66,10 +65,10 @@ Route::post('invio/mail', [MailController::class, 'sendMail'])->name('send.mail'
 
 //RICERCA ANNUNCIO
 
-Route::get('/ricerca/annuncio',[FrontController::class,'searchAnnouncements'])->name('announcements.search');
+Route::get('/ricerca/annuncio', [FrontController::class, 'searchAnnouncements'])->name('announcements.search');
 
 Route::post('/lingua/{lang}', [FrontController::class, 'setLanguage'])->name('set_language_locale');
 
-Route::middleware(['SetLocaleLang'])->group(function(){
-    Route::get('/',[FrontController::class, 'welcome'])->name('welcome');
-});
+//Route::middleware(['setLocaleLang'])->group(function () {
+   // Route::get('/', [FrontController::class, 'welcome'])->name('welcome');
+//});
