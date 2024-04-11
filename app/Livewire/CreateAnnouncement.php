0c4913957c091e9c98df7a2d5f6a6e6f73ba9bc3
Naizky <samulomba67@gmail.com>
2024-04-11 19:33:42 +0200

    google

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisonSafeSearch;
 use App\Models\User;
 use Livewire\Component;
@@ -85,6 +86,7 @@ class CreateAnnouncement extends Component
 
                     dispatch(new ResizeImage($newImage->path, 400, 300));
                     dispatch(new GoogleVisonSafeSearch($newImage->id));
+                    dispatch(new GoogleVisionLabelImage($newImage->id));
             } 
             File::deleteDirectory(storage_path('app/livewire-tmp'));
         }
